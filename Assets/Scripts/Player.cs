93603507b8d70ea2872151d81c86ad7cad3c0d08
연훈 배 <sbaru1997@naver.com>
2021-01-06 02:40:54 +0900

    Day4 simple jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,32 +5,67 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    private Animator Anim;
-
     public float speed;
     float hAxis;
     float vAxis;
     bool wDown;
+    bool jDown;
+
+    bool isJump;
 
     Vector3 moveVec;
 
-    void Start()
+    Animator Anim;
+    Rigidbody rigid;
+
+    void Awake()
     {
         Anim = gameObject.GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-
+        GetInput();
+        Move();
+        Jump();
+        Turn();
+    }
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
+        jDown = Input.GetButtonDown("Jump");
+    }
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-            moveVec = new Vector3(hAxis, 0, vAxis).normalized;
-            transform.position += moveVec * speed * Time.deltaTime;
+        transform.position += moveVec * speed * (wDown ? 1.5f : 0.8f) * Time.deltaTime;
 
         Anim.SetBool("isWalk", moveVec != Vector3.zero);
         Anim.SetBool("isRun", wDown);
+    }
+    void Turn()
+    {
+        transform.LookAt(transform.position + moveVec);
+    }
+    void Jump()
+    {
+        if (jDown && !isJump)
+        {
+            Anim.SetTrigger("doJump");
+            rigid.AddForce(Vector3.up * 5, ForceMode.Impulse);
+            isJump = true;
+        }
+    }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            isJump = false;
+        }
     }
 }
